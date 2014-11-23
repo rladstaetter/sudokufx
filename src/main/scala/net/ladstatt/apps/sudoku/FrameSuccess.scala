bf0f32b6888aa7c5e79b3b42964e3d8b6c93e8cd
Robert Ladstätter <rladstaetter@gmail.com>
2014-11-23 16:21:49 +0100

    not needed anymore

--- src/main/scala/net/ladstatt/apps/sudoku/FrameSuccess.scala
+++
@@ -1,8 +0,0 @@
-package net.ladstatt.apps.sudoku
-
-/**
- * Created by lad on 26.10.14.
- */
-case class FrameSuccess(detectedCells: Cells,
-                        digitSolution: Option[SudokuDigitSolution],
-                        solutionCells: Option[Cells])
