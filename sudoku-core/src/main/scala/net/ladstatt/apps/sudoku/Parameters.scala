49ac096f4680bb50ad8be31f66a5c92b98df5d2c
Robert Ladstätter <rladstaetter@gmail.com>
2015-03-15 22:48:53 +0100

    moved TemplateLibrary in its own file

--- sudoku-core/src/main/scala/net/ladstatt/apps/sudoku/Parameters.scala
+++ sudoku-core/src/main/scala/net/ladstatt/apps/sudoku/Parameters.scala
@@ -1,53 +1,5 @@
 package net.ladstatt.apps.sudoku
 
-import java.io.InputStream
-
-import net.ladstatt.opencv.OpenCV
-import org.opencv.core.{Mat, Size}
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.io.Source
-
-object TemplateLibrary {
-  val (templateWidth, templateHeight) = (25.0, 50.0)
-  val templateSize = new Size(templateWidth, templateHeight)
-  lazy val sudokuTemplateSize = new Size(templateWidth * Parameters.ssize, templateHeight * Parameters.ssize)
-
-  lazy val templateCorners = OpenCV.mkCorners(sudokuTemplateSize)
-  var getResourceAsStream: String => InputStream = getClass.getResourceAsStream
-  var templateResource: String = "/net/ladstatt/apps/sudokufx/templates.csv"
-
-  lazy val asSeq: Seq[Mat] = {
-    val digits: Seq[Array[Int]] =
-      Source.fromInputStream(getResourceAsStream(templateResource)).getLines().map(l => l.split(",").map(e => if (e == "0") 0 else 255)).toSeq
-
-    digits.map(OpenCV.toMat(_, templateSize))
-  }
-
-
-  /**
-   * given a template library, match the given contour to find the best match. this function takes around 1 ms
-   *
-   * @return
-   */
-  def detectNumber(candidate: Mat): Future[(SNum, SHitQuality)] = {
-    val resizedCandidate = OpenCV.resize(candidate, TemplateLibrary.templateSize) // since templates are 25 x 50
-    val matchHaystack: (SIndex, SudokuCanvas) => Future[(SIndex, SHitQuality)] = OpenCV.matchTemplate(resizedCandidate, _: Int, _: Mat)
-
-
-    val result: Future[(SIndex, SHitQuality)] =
-      for {s <- Future.sequence(for {(needle, number) <- TemplateLibrary.asSeq.zipWithIndex} yield
-      for {(number, quality) <- matchHaystack(number + 1, needle)} yield (number, quality))
-      } yield s.toSeq.sortWith((a, b) => a._2 < b._2).head
-
-
-    result
-  }
-
-
-}
-
 
 object Parameters {
 
