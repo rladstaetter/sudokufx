4a76d1b7299768b5d01645d2c8d2b3c7e87e69fc
Robert Ladstaetter <rladstaetter@gmail.com>
2016-05-15 20:40:04 +0200

    simplifying things

--- sudoku-core/src/main/scala/net/ladstatt/sudoku/package.scala
+++ sudoku-core/src/main/scala/net/ladstatt/sudoku/package.scala
@@ -9,19 +9,21 @@ import scala.concurrent.Future
   */
 package object sudoku {
 
-  type SHitQuality = Double
-
   //
   // contains 81 elements, on each position the number of hits for the digit is detected.
   // this means if for a cell 2 times a 3 is detected, the Map looks like this:
   // Map(... PosAtSudoku -> Map(3 -> 2) ...)
   //
   type HitCounters = Map[Int, Map[Int, Int]]
-  type DetectFn = Mat => Future[(Int, SHitQuality)]
+
+  // a function which returns a detected number and a quality thereof
+  type DetectFn = Mat => Future[(Int, Double)]
+
+
   /**
     * records for each number from 0 to 9 the best hit (quality) along with its digital data
     */
-  type DigitLibrary = Map[Int, (SHitQuality, Option[Mat])]
+  type DigitLibrary = Map[Int, (Double, Option[Mat])]
   // TODO solution should be an array of Int or a string with 81 entries
   // the graphical representation should be a single mat with the matrixes edited inline
   // (meaning the matrix represents the whole canvas and the app is working with submatrixes
